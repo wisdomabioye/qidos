969f7cb52437e43ebb1d3b0f043ea11e01b86d38
wisdomabioye <wisdomabioye@gmail.com>
2020-07-01 15:56:52 +0100

    add bitcoin price change update with setTimeout

--- script/main.js
+++ script/main.js
@@ -1,13 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
 	getBitcoinPrices();
+	writeCurrentYear();
 })
 
 function getBitcoinPrices() {
 	var dollar = "https://api.coinbase.com/v2/prices/spot?currency=USD";
 	var naira = "https://api.coinbase.com/v2/prices/spot?currency=NGN";
 
-	fetch(dollar).then(toJson).then(price => writeHtml(".btc-dollar", parseFloat(price.data.amount).toFixed(2)))
-	fetch(naira).then(toJson).then(price => writeHtml(".btc-naira", parseFloat(price.data.amount).toFixed(2)))
+	fetch(dollar)
+	.then(toJson)
+	.then(price => writeHtml(".btc-dollar", parseFloat(price.data.amount).toFixed(2)))
+	.then(() => fetch(naira))
+	.then(toJson)
+	.then(price => writeHtml(".btc-naira", parseFloat(price.data.amount).toFixed(2)))
+	.catch(console.log)
+	.finally(() => setTimeout(getBitcoinPrices, 5000))
 }
 
 
@@ -22,3 +29,9 @@ function writeHtml(el, content) {
 		element.textContent = content;
 	}
 } 
+
+function writeCurrentYear() {
+	var el = document.querySelector(".current-year");
+
+	el.innerHTML = new Date().getFullYear();
+}
